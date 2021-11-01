4a4b25eab1ad9830b38b7cc93e9ad569e7f990cf
rh2975 <rahat2975134@gmail.com>
2021-11-02 04:20:48 +0600

    shows (components) added

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import {View} from 'react-native';
 import Home from './screens/Home';
 
 const App = () => {
-  return <Home />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Home></Home>
+    </View>
+  );
 };
 
 export default App;
--- services/services.js
+++ services/services.js
@@ -2,12 +2,9 @@ import axios from 'axios';
 
 const apiUrl = 'https://api.themoviedb.org/3';
 const apiKey = 'api_key=8c247ea0b4b56ed2ff7d41c9a833aa77';
-// const apiKey = 'api_key=fd11bc4c510ce3b777704b1648bd4234';
-
 // Get Popular Movies
 export const getPopularMovies = async () => {
   const resp = await axios.get(`${apiUrl}/movie/popular?${apiKey}`);
-  console.log(resp);
   return resp.data.results;
 };
 
@@ -20,5 +17,21 @@ export const getUpcomingMovies = async () => {
 // Get Popular TV
 export const getPopularTv = async () => {
   const resp = await axios.get(`${apiUrl}/tv/popular?${apiKey}`);
+  return resp.data.results;
+};
+
+// Get Family Movies
+export const getFamilyMovies = async () => {
+  const resp = await axios.get(
+    `${apiUrl}/discover/movie?${apiKey}&with_genres=10751`,
+  );
+  return resp.data.results;
+};
+
+// Get Documnetery Movies
+export const getDocumentaryMovies = async () => {
+  const resp = await axios.get(
+    `${apiUrl}/discover/movie?${apiKey}&with_genres=99`,
+  );
   return resp.data.results;
 };
