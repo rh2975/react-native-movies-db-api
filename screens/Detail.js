664ec01c2d2240847f4a36e792cef17e5ba3027e
rh2975 <rahat2975134@gmail.com>
2021-11-04 05:10:25 +0600

    details updated

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,11 +1,122 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  StyleSheet,
+  ScrollView,
+  Image,
+  Dimensions,
+  ActivityIndicator,
+  Text,
+  View,
+} from 'react-native';
+import StarRating from 'react-native-star-rating';
+import {getMovie} from '../services/services';
+import dateFormat from 'dateformat';
+
+const placeholderImage = require('../assets/images/placeholder.png');
+const height = Dimensions.get('screen').height;
+
 const Detail = () => {
+  const movieId = 1;
+
+  const [movieDetail, setMovieDetail] = useState();
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    getMovie(movieId).then(movieData => {
+      setMovieDetail(movieData);
+      setLoaded(true);
+    });
+  }, [movieId]);
+
   return (
     <React.Fragment>
-      <Text>{'Movie Detail'}</Text>
+      {loaded && (
+        <View>
+          <ScrollView>
+            <Image
+              resizeMode="cover"
+              style={styles.image}
+              source={
+                movieDetail.poster_path
+                  ? {
+                      uri:
+                        'https://image.tmdb.org/t/p/w500' +
+                        movieDetail.poster_path,
+                    }
+                  : placeholderImage
+              }
+            />
+            <View style={styles.container}>
+              <Text style={styles.movieTitle}>{movieDetail.title}</Text>
+              {movieDetail.genres && (
+                <View style={styles.genresContainer}>
+                  {movieDetail.genres.map(genre => {
+                    return (
+                      <Text style={styles.genre} key={genre.id}>
+                        {genre.name}
+                      </Text>
+                    );
+                  })}
+                </View>
+              )}
+              <StarRating
+                disabled={true}
+                maxStars={5}
+                starSize={30}
+                rating={movieDetail.vote_average / 2}
+                fullStarColor={'gold'}
+              />
+              <Text style={styles.overview}>{movieDetail.overview}</Text>
+
+              <Text style={styles.release}>
+                {'Release date: ' +
+                  dateFormat(movieDetail.release_date, 'mmmm dS, yyyy')}
+              </Text>
+            </View>
+          </ScrollView>
+        </View>
+      )}
+      {!loaded && <ActivityIndicator size="large" />}
     </React.Fragment>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    height: height / 2.5,
+  },
+  movieTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  genresContainer: {
+    flexDirection: 'row',
+    alignContent: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  genre: {
+    marginRight: 10,
+    fontWeight: 'bold',
+  },
+  overview: {
+    padding: 15,
+  },
+  release: {
+    fontWeight: 'bold',
+  },
+  videoModal: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default Detail;
