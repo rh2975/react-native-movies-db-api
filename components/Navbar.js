72622de0029b3c85bd6db975a9d83026d5d78c9d
rh2975 <rahat2975134@gmail.com>
2021-11-06 05:51:09 +0600

    added theme colors

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,6 +8,7 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import PropTypes from 'prop-types';
+import Colors from '../theme/Colors';
 
 const propTypes = {
   main: PropTypes.bool,
@@ -32,7 +33,7 @@ class Navbar extends React.PureComponent {
               onPress={() => {
                 navigation.navigate('Search');
               }}>
-              <Icon name={'search-outline'} size={30} color={'#fff'} />
+              <Icon name={'search-outline'} size={30} color={Colors.white} />
             </TouchableOpacity>
           </View>
         ) : (
@@ -41,7 +42,7 @@ class Navbar extends React.PureComponent {
               onPress={() => {
                 navigation.goBack();
               }}>
-              <Icon name={'chevron-back'} size={40} color={'#fff'} />
+              <Icon name={'chevron-back'} size={40} color={Colors.lightGray} />
             </TouchableOpacity>
           </View>
         )}
